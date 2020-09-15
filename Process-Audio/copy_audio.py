145159ce60b7bb43667e895df9ac9394ab2ac3b6
mhsjacoby <margarite.jacoby@colorado.edu>
2020-09-15 15:52:53 -0700

    add gen_argparse file

--- Process-Audio/copy_audio.py
+++ Process-Audio/copy_audio.py
@@ -2,7 +2,7 @@
 copy_audio.py
 Author: Sin Yong Tan 2020-09-10
 Updates by Maggie Jacoby
-	2020-09-14: give end date
+	2020-09-15: move argument parser to separate file
 
 This code takes in image occupancy prediction files and copies audio to another folder for human verification and training
 	
@@ -22,31 +22,13 @@ from natsort import natsorted
 
 import shutil
 
+from gen_argparse import *
 from my_functions import *
 
 
 if __name__ == '__main__':
+	# uses arguments specifed by gen_argparse.py
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-path','--path', default='AA', type=str, help='path of stored data')
-	parser.add_argument('-hub', '--hub', default='', type=str, help='if only one hub... ')
-	parser.add_argument('-save_location', '--save', default='', type=str, help='location to store files (if different from path')
-	parser.add_argument('-start_date', '--start', default='', type=str, help='type day to start')
-	parser.add_argument('-end_date', '--end', default='', type=str, help='type day to end')
-	parser.add_argument('-img_name', '--img_name', default='img-unpickled', type=str, help='name of files containing raw images')
-
-	args = parser.parse_args()
-
-	path = args.path
-	
-	start_date = args.start
-	end_date = args.end
-	home_system = os.path.basename(path.strip('/'))
-	H = home_system.split('-')
-	H_num, color = H[0], H[1][0].upper()
-	save_root = os.path.join(args.save, home_system,'Auto_Labled') if len(args.save) > 0 else os.path.join(path, 'Auto_Labeled')
-
-	hubs = [args.hub] if len(args.hub) > 0 else sorted(mylistdir(path, bit=f'{color}S', end=False))
 	print(f'List of Hubs: {hubs}')
 
 	mode = 'audio'
@@ -54,14 +36,22 @@ if __name__ == '__main__':
 	for hub in hubs:
 		infer_csv_path = os.path.join(path,'Inference_DB', hub, 'img_inf', '*.csv')
 
-		save_path = make_storage_directory(os.path.join(save_root, f'{mode}_{hub}'))
+		save_path = make_storage_directory(os.path.join(save_root,'Auto_Labled', f'{mode}_{hub}'))
 		days = [day for day in sorted(glob(infer_csv_path))]
+
+		if len(days) == 0:
+			print(f'No days in folder: {infer_csv_path}. Exiting program.')
+			sys.exit()
+
 		end_date =  os.path.basename(days[-1]).strip('.csv') if not end_date else end_date
 		days = [day for day in days if os.path.basename(day).strip('.csv') <= end_date]
+		
+		print(f'Number of days: {len(days)}')
 
 		for day in days:
 			day_name = os.path.basename(day).strip('.csv')
-			all_data = pd.read_csv(day,index_col=0) # read dat
+
+			all_data = pd.read_csv(day,index_col=0)
 			all_data.index = pd.to_datetime(all_data.index)
 			all_data["day"] = all_data.index.date
 			all_data["time"] = all_data.index.time
@@ -72,7 +62,6 @@ if __name__ == '__main__':
 			copy_paths_root = os.path.join(path, hub, mode)
 			copy_paths = [os.path.join(copy_paths_root,f"{data['day'][i]}",f"{data['time'][i]}".replace(":","")[:4],"*"+f"{data['time'][i]}".replace(":","")+"*") for i in range(len(data['time']))]
 
-			# sys.exit()
 			for copy_path in copy_paths:
 				src = natsorted(glob(copy_path))
 
